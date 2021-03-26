96986caa002b34fd71dfd686deb93a841ab06167
Marcel-Brian Wilkowsky <marcel.wilkowsky@uni-paderborn.de>
2021-03-26 12:37:01 +0100

    UnixLogin SecurityTypes

--- jupyter_desktop/__init__.py
+++ jupyter_desktop/__init__.py
@@ -32,7 +32,7 @@ def setup_desktop():
         '-verbose',
         '-xstartup', os.path.join(HERE, 'share/xstartup'),
         '-geometry', '1680x1050',
-        '-SecurityTypes', 'None',
+        '-SecurityTypes', 'UnixLogin',
         '-fg',
         ':1',
     ]))
