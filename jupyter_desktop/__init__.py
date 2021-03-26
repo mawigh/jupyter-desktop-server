89d2f0576d4123f3c1d8b53d2cb456cb2bbf12a5
Marcel-Brian Wilkowsky <marcel.wilkowsky@uni-paderborn.de>
2021-03-26 19:55:33 +0100

    Test

--- jupyter_desktop/__init__.py
+++ jupyter_desktop/__init__.py
@@ -32,7 +32,6 @@ def setup_desktop():
         '-verbose',
         '-xstartup', os.path.join(HERE, 'share/xstartup'),
         '-geometry', '1680x1050',
-        '-SecurityTypes', 'UnixLogin',
         '-fg',
     ]))
     return {
